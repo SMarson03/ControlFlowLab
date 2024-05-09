3cff48a57b1c4ad760896c5462fcd6981ac49bd0
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-09 15:35:41 -0400

    more

--- app.js
+++ app.js
@@ -7,24 +7,30 @@ The area in which the plants are contained is circular, with a radius of 5 meter
 The formula for calculating the area of a circle is PI multiplied by the radius, 
 squared: const PI = 3.1415; const area = PI * radius * radius*/
 
-const PI = 3.1415
-const radius = 5
+const PI = 3.1415;
+const radius = 5;
 const area = PI * radius * radius
+console.log(area)
 
-//Each plant requires a minimum space of 0.8 square meters.
-const minSpace = .8
+//Each plant requires a minimum space of 0.8 square meters
 
-//The area is starting with 20 plants.
-const plant = 20
-//The plants double in number every week.
+const minSpace = .8;
+const plant = 20;
 
-/*Predict the plant growth after a specific number of weeks.
-Implement control flow to make decisions on whether the plants should be:
-Pruned, to stop them from exceeding the capacity of the garden. This condition 
-should be met if the plant count after the given number of weeks is greater than 
-80% of the maximum capacity of the garden. Monitored, if they are growing at an 
-acceptable rate. This condition should be met if the plant count is between 50% 
-and 80% of the maximum capacity of the garden after the given number of weeks.
-Planted, if there is room to plant more plants. This condition should be met if the 
-plant count after the given number of weeks is less than 50% of the maximum capacity of 
-the garden./
+//The area is starting with 20 plants
+const perPlant = minSpace/ plant
+console.log(perPlant)
+
+//The plants double in number every week
+
+const growthWeek1 = 20
+const growthWeek2 = growthWeek1*2 
+const growthWeek3 = growthWeek2*2
+console.log(growthWeek1)
+console.log(growthWeek2)
+console.log(growthWeek3)
+
+//implement control flow to make decisions on whether the plants should be:
+//Pruned, to stop them from exceeding the capacity of the garden.
+//This condition should be met if the plant count after the given number of 
+//weeks is greater than 80% of the maximum capacity of the garden.
