64d4b707d75344e93fd5793c79fb3a76f6218fb8
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-05-10 20:55:17 -0400

    updated growing pains

--- app.js
+++ app.js
@@ -14,52 +14,55 @@ console.log(area)
 
 //Each plant requires a minimum space of 0.8 square meters
 
-const minSpace = .8;
+const plantSpace = .8;
 const plant = 20;
-
-//The area is starting with 20 plants
-const perPlant = minSpace/ plant
-console.log(perPlant)
+const maxCapacity = area / plantSpace;
+console.log(maxCapacity)
 
 //The plants double in number every week
+function Growth (weeks){
+    const plantCount = plant * Math.pow(2, weeks);
+    if (plantCount > maxCapacity *.8){
+        console.log(`Week ${weeks}: Prune the plants. Too many plants!`);
+        }
 
-const growthWeek1 = 20
-const growthWeek2 = growthWeek1*2 
-const growthWeek3 = growthWeek2*2
-console.log(growthWeek1)
-console.log(growthWeek2)
-console.log(growthWeek3)
+        else if(
+            plantCount >= maxCapacity * .5 &&
+            plantCount <= maxCapacity * .8
+        ){
+            console.log(`Week ${weeks}: Monitor the plants. They are within limits.`            );
+        }
+    else {
+        console.log(`Week ${weeks}: Plant more. There is room.`);
+    }
+}
+ Growth(1)
+ Growth(2)
+ Growth(3)
+
+//The area is starting with 20 plants
 
 //implement control flow to make decisions on whether the plants should be:
 //Pruned, to stop them from exceeding the capacity of the garden.
 //This condition should be met if the plant count after the given number of 
-//weeks is greater than 80% of the maximum capacity of the garden.
+//weeks is greater than 80% of the maximum capacity of the garden
 
-if (pruned = growthWeek1 >= minSpace){
-    console.log("Time to prune");
-   
-    } else {
-        console.log("Do not prune")
-    }
-
-    if (pruned = growthWeek2 >= minSpace){
-        console.log("Time to prune");
-       
-        } else {
-            console.log("Do not prune")
+//Planted, if there is room to plant more plants. This condition should be met 
+//if the plant count after the given number of weeks is less than 50% of the maximum capacity of the garden.
+function Growth (weeks){
+    const plantCount = plant * Math.pow(2, weeks);
+    if (plantCount > maxCapacity *.5){
+        console.log(`Week ${weeks}: No more!`);
+        }else if(
+            plantCount >= maxCapacity * .5 &&
+            plantCount <= maxCapacity * .5
+        ){
+            console.log(`Week ${weeks}: Monitor the plants. They are within limits.`            );
         }
-
-        if (pruned = growthWeek3 >= minSpace){
-            console.log("Time to prune");
-           
-            } else {
-                console.log("Do not prune")
-            }
-    
-            const monitored = .5
-          if (pruned2 = growthWeek1 >= monitored >= minSpace){
-                console.log("Time to prune");
-               
-                } else {
-                    console.log("Do not prune")
-                }
+    else {
+        console.log(`Week ${weeks}: Plant more. There is room.`);
+    }
+}
+ Growth(1)
+ Growth(2)
+ Growth(3)
---
+++ test.js
@@ -0,0 +1,22 @@
+function Prune(){
+    if (growthWeek1 >= minSpace){
+        console.log("Time to prune");
+       
+        } else {
+            console.log("Do not prune")
+        }
+    if (growthWeek2 >= minSpace){
+            console.log("Time to prune");
+           
+            } else {
+                console.log("Do not prune")
+            }
+    
+    if (growthWeek3 >= minSpace){
+                console.log("Time to prune");
+               
+                } else {
+                    console.log("Do not prune")
+                }
+        
+}
